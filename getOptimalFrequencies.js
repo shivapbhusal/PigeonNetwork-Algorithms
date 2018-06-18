20ce750d6af896391068f317a6fe706f68019179
shivapbhusal <civabhusal@gmail.com>
2018-06-18 19:33:55 -0400

    update comments / logic in optimalFrequencies

--- getOptimalFrequencies.js
+++ getOptimalFrequencies.js
@@ -1,20 +1,18 @@
 /* JavaScript code to get optimal frequency.
  * Suppose our frequencies are [ 0.9, 0.8, 0.5, 10.5, 11.6 ] 
  * We want to make their ratio in such a way that they are integers and their ratio is closest to perfect.
- */
+ * Special case : 0.1, 0.2 etc.. 
+ * In the special case, New frequencys is 0 and it becomes 0 / 0.1. So it becomes [1-0 ] == 1. It should have been taken care of.
+*/
 
 function isConditionSatisfied ( frequency, delta )
 {
-	let flag = false;
+	let flag = true;
 	frequency.forEach(function(value){
-		console.log(Math.abs(1-(Math.round(Number.parseFloat(value)) / value)));
+		// console.log(Math.abs(1-(Math.round(Number.parseFloat(value)) / value)));
 		if (Math.abs(1-(Math.round(Number.parseFloat(value)) / value)) > delta)
 		{
 			flag = false;
-			return flag;
-		}
-		else {
-			flag = true;
 		}
 
 	});
@@ -23,6 +21,11 @@ function isConditionSatisfied ( frequency, delta )
 
 }
 
-frequency = [0.9,0.1,0.08,10.01, 11.91]
+frequency = [0.9,0.1,0.08,10.01, 11.91]; // This should be true
+console.log(isConditionSatisfied(frequency,0.2));
 
+frequency = [0.7,0.1,0.08,10.01, 11.91]; // This should be false
+console.log(isConditionSatisfied(frequency,0.2));
+
+frequency = [1.3,0.1,0.08,10.01, 11.91]; // This should be false
 console.log(isConditionSatisfied(frequency,0.2));
