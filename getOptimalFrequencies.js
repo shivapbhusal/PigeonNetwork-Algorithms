55dcb10957c1e4e2e20c8123da9c15548da1ce13
shivapbhusal <civabhusal@gmail.com>
2018-06-18 18:45:44 -0400

    add draft for getOptimalFrequency

--- getOptimalFrequencies.js
+++ getOptimalFrequencies.js
@@ -1,3 +1,10 @@
 /* JavaScript code to get optimal frequency.
- *
+ * Suppose our frequencies are [ 0.9, 0.8, 0.5, 10.5, 11.6 ] 
+ * We want to make their ratio in such a way that they are integers and their ratio is closest to perfect.
  */
+
+function isConditionSatisfied ( frequency, delta )
+{
+	flag = false;
+
+}
