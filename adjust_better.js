c46958b18ccb91ad715c9767e50a2996c6dfffae
shivapbhusal <civabhusal@gmail.com>
2018-06-22 17:56:36 -0400

    refactor, update

--- adjust_better.js
+++ adjust_better.js
@@ -1,11 +1,9 @@
 /*
  Nodejs script to adjust the perfect randomness.
- Brute Force solution -- Run the loop until the condition is satisfied.
-
- Optimized solution: Keep the generate random floats between two close floats.This will make sure that distance from host host and the lamda remain almost similar for all the foreign hosts.
- Optimized solution is nothing but a careful bruteforce which heavily reduces the no of iterations needed to get to the required result.
-
+ Optimized solution: First get s, then get random x and y. 
+ Finally adjust lambda according to s.
 */
+
 function calculateDistance(homeHost, foreignHost){
 	let distance = Math.sqrt(Math.pow(homeHost.get("x")-foreignHost.get("x"),2)+ 
 		Math.pow(homeHost.get("y") - foreignHost.get("y"),2));
@@ -16,38 +14,64 @@ function generateRandomFloat(min, max){
       return parseFloat((min+Math.random()*(max - min)).toFixed(4));
 }
 
+// Function to compute s in a set of foreignHosts
+function calculateS(foreignHosts)
+{
+	let minRatio = Infinity;
+	let maxRatio = 0;
+	for (let foreignHost of foreignHosts){
+	distance=calculateDistance(homeHost, foreignHost);
+        lambda = foreignHost.get("lambda");
+        ratio = lambda / distance;
+	// console.log(ratio);
+	if (ratio <minRatio){
+		minRatio = ratio;
+	}
+	if (ratio >maxRatio){
+		maxRatio = ratio;
+	}
+}
+
+s = maxRatio / minRatio;
+return s;
+}
+
 // Get the number of nodes. If there is no argument, exit the program.
 if (!process.argv[2]){
 	console.log("Enter the no of nodes");
 	process.exit();
 }
 
+
 const N = process.argv[2]; // No of foreign host.
-const S_MAX = 1.1;
+const S_INDEX = 1.1;
+
+// Create a Home Host.
 let homeHost = new Map();
 homeHost.set("x",0);
 homeHost.set("y",0);
 
 let foreignHosts = [];
+
+// Get the first Node.
 let first_node = new Map();
 const S_FOR_FIRST_NODE = parseFloat(Math.random().toFixed(4));
 let x = Math.random();
 let y = Math.random();
-let distance = Math.sqrt((-homeHost.get("x"),2)+
-		       Math.pow(y - homeHost.get("y"),2));
+let distance = Math.sqrt(Math.pow(x-homeHost.get("x"),2)+Math.pow(y - homeHost.get("y"),2));
 let lambda = S_FOR_FIRST_NODE * distance;
 first_node.set("x", x);
 first_node.set("y", y);
 first_node.set("lambda", lambda);
-
 foreignHosts.push(first_node);
 
+// Get rest of the Nodes 
 for (let i=0; i<N-1; i+= 1){
 	const temp = new Map();
 	let x = Math.random();
 	let y = Math.random();
-	let s = generateRandomFloat(S_FOR_FIRST_NODE, S_FOR_FIRST_NODE*S_MAX);
-	let distance = Math.sqrt((x-homeHost.get("x"),2)+
+	let s = generateRandomFloat(S_FOR_FIRST_NODE, S_FOR_FIRST_NODE*S_INDEX);
+	let distance = Math.sqrt(Math.pow(x-homeHost.get("x"),2)+
 		       Math.pow(y - homeHost.get("y"),2));
 	let lambda = s * distance;
 	temp.set("x", x);
@@ -58,4 +82,7 @@ for (let i=0; i<N-1; i+= 1){
 
 console.log(foreignHosts);
 
+// Test the s value for generated set of ForeignHosts. 
+
+console.log("S:",calculateS(foreignHosts));
 
