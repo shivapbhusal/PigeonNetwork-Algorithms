c2d5719c257b736e772d8753b57752a899509d93
shivapbhusal <civabhusal@gmail.com>
2018-06-23 11:16:38 -0400

    update results

--- results/computeAvgDelay.py
+++ results/computeAvgDelay.py
@@ -8,17 +8,17 @@ import json
 
 result_file = sys.argv[1]
 
-avgResults = {"rr":0, "drr": 0, "prob":0, "det":0}
-with open(result_file) as jsonData:
+avgResults = {"rr":0, "drr":0, "prob":0, "det":0}
+
+with open(result_file, "r") as jsonData:
     parsedJson = json.load(jsonData)
-    print(parsedJson)
     for resultId in parsedJson:
         try:
             result = parsedJson[resultId]
             avgResults["rr"] += result["rr"]
-            avgResults["drr"] += results["drr"]
-            avgresults["prob"] += results["prob"]
-            avgResults["det"] += results["det"]
+            avgResults["drr"] += result["drr"]
+            avgResults["prob"] += result["prob"]
+            avgResults["det"] += result["det"]
         except KeyError:
             continue
 
