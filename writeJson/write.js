88746d41306fba46296a231fc5902f70e95df36a
shivapbhusal <civabhusal@gmail.com>
2018-06-25 14:35:53 -0400

    update write.js

--- writeJson/write.js
+++ writeJson/write.js
@@ -3,14 +3,19 @@
  Example : Borrowed, adpated from https://gyandeeps.com/json-file-write/
  */
 var fs = require("fs");
-var sampleObject = {
-    a: 1,
-    b: 2,
-    c: {
-        x: 11,
-        y: 22
+
+function strMapToObj(strMap) {
+    let obj = Object.create(null);
+    for (let [k,v] of strMap) {
+        // We don’t escape the key '__proto__'
+        // which can cause problems on older engines
+        obj[k] = v;
     }
-};
+    return obj;
+}
+
+myMap = new Map().set("rr",1).set("prob",2).set("det",3);
+sampleObject = strMapToObj(myMap);
 
 fs.writeFile("object.json", JSON.stringify(sampleObject, null, 4), (err) => {
     if (err) {
