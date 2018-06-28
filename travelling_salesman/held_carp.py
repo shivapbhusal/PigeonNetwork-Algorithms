c01ed7df3846291c3f76dab4c93c1360b8fe3d81
shivapbhusal <civabhusal@gmail.com>
2018-06-28 12:09:52 -0400

    update held_carp again

--- travelling_salesman/held_carp.py
+++ travelling_salesman/held_carp.py
@@ -9,66 +9,74 @@ import itertools
 import random
 import sys
 
-def findPath(distances, start):
+def findPath(dists, start):
     '''
     Inputs: Distances: nested Key value pair
     Start: A string indicating the Node to start with
     Returns a tuple with cheapest path and total cost.
     '''
-    costInfo = dict()
-    nodes = distances.keys()
-    N = len(nodes)
-    C = dict()
+    nodes = dists.keys()
+    print(nodes)
+    n = len(nodes)
+    C = {}
 
-    for k in range(1,N):
-        C[(1 << k, k)] = (distances[nodes[0]][nodes[k]],0) #Get the kth element from list and look in the distances dictionary.
+    for k in range(1, n):
+        C[(1 << k, k)] = (dists[0][k], 0)
 
-    for size in range(2,N):
-        for subset in itertools.combinations(range(1,n),size):
+    # Iterate subsets of increasing length and store intermediate results
+    # in classic dynamic programming manner
+    for subset_size in range(2, n):
+        for subset in itertools.combinations(range(1, n), subset_size):
+            # Set bits for all nodes in this subset
             bits = 0
             for bit in subset:
                 bits |= 1 << bit
 
-        for k in subset:
-            prev = bits & ~(1 << k)
+            # Find the lowest cost to get to this subset
+            for k in subset:
+                prev = bits & ~(1 << k)
 
-            res = []
-            for m in subset:
-                if m==0 or m==k:
-                    continue
-                res.append((C[(prev, m)][0]+ distances[m][k],m))
-            C[(bits,k)] = min(res)
+                res = []
+                for m in subset:
+                    if m == 0 or m == k:
+                        continue
+                    res.append((C[(prev, m)][0] + dists[m][k], m))
+                C[(bits, k)] = min(res)
 
-    bits = (2**n -1) -1
+    # We're interested in all bits but the least significant (the start state)
+    bits = (2**n - 1) - 1
 
+    # Calculate optimal cost
     res = []
-    for k in range(1,n):
-        res.append((C[bits, k)][0] + distances[k][0],k))
-
+    for k in range(1, n):
+        res.append((C[(bits, k)][0] + dists[k][0], k))
     opt, parent = min(res)
 
+    # Backtrack to find full path
     path = []
-
-    for i in range(n-1):
+    for i in range(n - 1):
         path.append(parent)
-        newBits = bits & ~(1 <<parent)
-        _, parent = C[(bits, parent )]
-        bits = newBits
+        new_bits = bits & ~(1 << parent)
+        _, parent = C[(bits, parent)]
+        bits = new_bits
 
+    # Add implicit start state
     path.append(0)
 
     return opt, list(reversed(path))
 
+    
+
 
 # A dictionary that keeps adjacency information and distances for all nodes.
 distances = {
-        "A":{"B":5, "C":10, "D":1,"E":10},
-        "B":{"A":5, "C":30,"D":20,"E":3},
-        "C":{"A":10,"B":30,"D":30,"E":50},
-        "D":{"A":3,"B":5,"C":40,"D":20},
+        0:{1:5,2:10, 3:1},
+        1:{0:5,2:30,3:20},
+        2:{0:10,1:30,3:30},
+        3:{0:30,1:5,2:40}
 }
 
-start = "A"
+start = 0
 
 print("Path:",findPath(distances,start))
 
