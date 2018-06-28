430d0e6423199ed09d022e2bfb827f6d6ee936cc
shivapbhusal <civabhusal@gmail.com>
2018-06-28 17:35:25 -0400

    update held_carp

--- travelling_salesman/held_carp.js
+++ travelling_salesman/held_carp.js
@@ -8,12 +8,39 @@ function getRandomInt(min, max){
 	return Math.floor(Math.floor(min + (Math.random())*(max-min))) 
 }
 
+function areEqual(array1, array2) {
+	// Checks if two arrays are equal in values.
+	if (array1.length != array2.length){
+		return false;
+	}
+	for (let i =0; i<array1.length; i++){
+		if (array1[i] != array2[i]){
+			return false;
+		}
+	}
+
+	return true;
+ }
+
+ function getValue(map, array){
+ 	// Get value from a Map where key is an array.
+	let ans;
+ 	map.forEach((value, key) => {
+ 		if (areEqual(array, key)){
+ 			ans = value;
+ 		}
+ 	});
+ 	if (ans) return ans;
+ 	else console.log("Key not found");
+ }
+
 function findPath(dists){
 	let n = dists.size;
 	arrayOfNodes = []
-	for (let i=0;i<n;i++){
+	for (let i=1;i<n;i++){
 		arrayOfNodes.push(i);
 	}
+	console.log(arrayOfNodes);
 
 	console.log(n);
 
@@ -27,11 +54,33 @@ function findPath(dists){
 	for (let subSize = 2; subSize < n; subSize++){
 		for(let subset of Array.from(iter.permutations(arrayOfNodes,subSize))){
 			let bits = 0;
+
+			for (let bit of subset){
+				bits |= 1 << bit;
+			}
+
+			for (let k of subset){
+				prev = bits & ~(1 << k);
+
+				res = []
+
+				for (let m of subset){
+					if (m ==0 || m ==k){
+						continue;
+					}
+					console.log(prev,m);
+					res.push([getValue(C,[prev,m])[0] + distances.get(m).get(k),m]);
+				}
+				C.set([bits, k],Math.min(...res));
+			}
+
 		}
 	}
 }
 
 
+
+// start of the test
 distances = new Map();
 const N_NODES = 4;
 const MAX_DIST = 20;
